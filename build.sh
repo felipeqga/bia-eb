a0409c5f02457f323ba67245f54c6ddfe5bd43d0
Felipe <felipequesado@gmail.com>
2024-04-15 14:30:18 -0300

    ---

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ docker build -t bia .
 docker tag bia:latest $ECR_ID/bia:$versao
 docker push $ECR_ID/bia:$versao
 rm .env 2> /dev/null
-#------GERA COMPOSE----------------
+#------GERA COMPOSE--------------------
 echo TAG=$versao >> .env
 echo ECR_ID1=$ECR_ID >> .env
 docker compose -f docker-compose-eb.yml config >> docker-compose-dev.yml
