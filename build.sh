ffc8b0edecd638972d90cc085d0e6584c09dffa8
felipeqga <40903953+felipeqga@users.noreply.github.com>
2024-04-15 13:16:31 -0300

    BUILD.SH FILE

--- build.sh
+++ build.sh
@@ -1,1 +1,22 @@
-aaaa
+versao1=$(git rev-parse HEAD | cut -c 1-7)
+echo $versao1
+data=$(date '+%Y-%m-%d')
+echo $data
+versao="${versao1}_${data}"
+echo $versao
+ECR_ID=557772028142.dkr.ecr.us-east-1.amazonaws.com
+echo $ECR_ID
+aws ecr get-login-password --region us-east-1 --profile bia-serverless | docker login --username AWS --password-stdin $ECR_ID
+docker build -t bia .
+docker tag bia:latest $ECR_ID/bia:$versao
+docker push $ECR_ID/bia:$versao
+rm .env 2> /dev/null
+#------GERA COMPOSE
+echo TAG=$versao >> .env
+docker compose -f docker-compose-eb.yml config >> docker-compose-dev.yml
+mv docker-compose-dev.yml docker-compose.yml
+#./gerar-compose.sh
+#------------
+rm bia-versao-*zip
+zip -r bia-versao-$versao.zip docker-compose.yml
+git checkout docker-compose.yml
