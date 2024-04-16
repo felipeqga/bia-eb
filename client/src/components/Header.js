0b209b07ee6c3d81a154cfc67f2949f0bfa51dca
Felipe <felipequesado@gmail.com>
2024-04-16 18:44:12 -0300

    --

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,7 +19,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 };
 
 Header.defaultProps = {
-  title: "BIA EB V6 com script",
+  title: "BIA EB V6 SALVANDO DADOS LOCAL(DOCKER)",
 };
 
 export default Header;
