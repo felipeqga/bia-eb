bd7ad3ef046a1e87dcdbcebfdc303a84d41f5ba5
Felipe <felipequesado@gmail.com>
2024-04-17 12:47:40 -0300

    --

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,7 +19,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 };
 
 Header.defaultProps = {
-  title: "BIA EB V3 - EB CLI",
+  title: "BIA EB V3 - EB CLI - VIA *CLOUDSHELL*",
 };
 
 export default Header;
